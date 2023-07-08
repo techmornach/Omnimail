f7351cce599b16e207a1b004f7ca72778187554a
Mornach <mornach@Mornachs-MacBook-Pro.local>
2023-07-08 07:33:08 +0100

    push

--- server/src/Contacts.ts
+++ server/src/Contacts.ts
@@ -1,63 +1,67 @@
-import * as path from "path"
-const Datastore = require("nedb");
+import path from "path";
+import Datastore from "nedb";
 
-export interface IContact{
-    _id?:number, name: string, email: string
+export interface IContact {
+    _id?: string | number;
+    name: string;
+    email: string;
 }
-export class Worker{
-    private db: Nedb;
+
+export class Worker {
+    private db: Datastore;
+
     constructor() {
         this.db = new Datastore({
-            filename:path.join(__dirname, "/contacts.db"),
-            autoload: true
-        })
-    }
-    public listContacts():Promise<IContact[]>{
-        return new Promise((resolve, reject)=>{
-            this.db.find({ }, (err:Error, docs: IContact[])=>{
-                if(err){
-                    reject(err);
-                }
-                else{
-                    resolve(docs)
-                }
-            })
-        })
-    }
-    public addContact(contact: IContact):Promise<IContact>{
-        return new Promise((resolve, reject)=>{
-            this.db.insert(contact, (err: Error|null, newDoc:IContact)=>{
-                if(err){
-                    reject(err)
-                }
-                else{
-                    resolve(newDoc)
-                }
-            })
-        })
-    }
-    public updateContact(id:string, contact:IContact):Promise<string>{
-        return new Promise((resolve, reject)=>{
-            this.db.update({_id: id}, contact,{ },(err: Error|null, numberOfUpdated: number)=>{
-                if(err){
-                    reject(err)
-                }
-                else{
-                    resolve("Updated")
-                }
-            })
-        })
+            filename: path.join(__dirname, "/contacts.db"),
+            autoload: true,
+        });
     }
 
-    public deleteContact(id: string):Promise<string>{
-        return new Promise((resolve, reject)=>{
-            this.db.remove({_id:id}, { }, (err: Error|null, inNumRemoved: number)=>{
-                if(err){
+    public listContacts(): Promise<IContact[]> {
+        return new Promise((resolve, reject) => {
+            this.db.find({}, (err: Error, docs: IContact[]) => {
+                if (err) {
                     reject(err);
-                }else{
+                } else {
+                    resolve(docs);
+                }
+            });
+        });
+    }
+
+    public addContact(contact: IContact): Promise<IContact> {
+        return new Promise((resolve, reject) => {
+            this.db.insert(contact, (err: Error | null, newDoc: IContact) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(newDoc);
+                }
+            });
+        });
+    }
+
+    public updateContact(id: string | number, contact: IContact): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.db.update({ _id: id }, contact, {}, (err: Error | null, numberOfUpdated: number) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve("Updated");
+                }
+            });
+        });
+    }
+
+    public deleteContact(id: string | number): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.db.remove({ _id: id }, {}, (err: Error | null, inNumRemoved: number) => {
+                if (err) {
+                    reject(err);
+                } else {
                     resolve("Deleted");
                 }
-            })
-        })
+            });
+        });
     }
 }
